1a5c1d0bbf3314d1948a711e714af6db9fc17785
Rendi <rendicrew96@gmail.com>
2023-04-01 13:11:43 +0700

    hide logging

--- server_process_http_secure.py
+++ server_process_http_secure.py
@@ -31,12 +31,12 @@ class ProcessTheClient(threading.Thread):
                     rcv = rcv+d
                     if rcv[-2:] == '\r\n':
                         # end of command, proses string
-                        logging.warning("data dari client: {}" . format(rcv))
+                        # logging.warning("data dari client: {}" . format(rcv))
                         hasil = httpserver.proses(rcv)
                         # hasil akan berupa bytes
                         # untuk bisa ditambahi dengan string, maka string harus di encode
                         hasil = hasil+"\r\n\r\n".encode()
-                        logging.warning("balas ke  client: {}" . format(hasil))
+                        # logging.warning("balas ke  client: {}" . format(hasil))
                         # hasil sudah dalam bentuk bytes
                         self.connection.sendall(hasil)
                         rcv = ""
@@ -70,8 +70,8 @@ class Server(Process):
             try:
                 self.secure_connection = self.context.wrap_socket(
                     self.connection, server_side=True)
-                logging.warning(
-                    "connection from {}".format(self.client_address))
+                # logging.warning(
+                #     "connection from {}".format(self.client_address))
                 clt = ProcessTheClient(
                     self.secure_connection, self.client_address)
                 clt.start()
