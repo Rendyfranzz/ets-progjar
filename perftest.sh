216ea3463bed382ca8224a5b1dba52fec32b958c
Rendi <rendicrew96@gmail.com>
2023-04-01 21:51:45 +0700

    fix typo

--- perftest.sh
+++ perftest.sh
@@ -2,7 +2,7 @@
 
 
 #non secure
-ab -n 100 -c <c> -s 99999  http://localhost:8887/testing.txt
+ab -n 1000 -c <c> -s 99999  http://localhost:8889/testing.txt
 
 #secure
 ab -n 1000 -c <c> -k -f SSL2 https://localhost:8443/testing.txt
